e78b2691381a68d83bad4c5872ea129f239d158a
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-09 13:30:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_hash = {}
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @@artists.push(artist)
+    @genre = genre
+    @@genres.push(genre)
+  end
+  
+  def self.count
+    return @@count
+  end
+  
+  def self.artists
+    @@artists.uniq!
+    return @@artists
+  end
+  
+  def self.genres
+    @@genres.uniq!
+    return @@genres
+  end
+  
+  def self.genre_count
+    @@genre_hash
+  end
+  
+end
